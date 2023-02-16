493fe4cb01ce6002ef9783d24a8f9654a78f8c0e
caoguzelmas <caoguzelmas@gmail.com>
2023-02-16 17:33:07 +0300

    dockerfile implemented

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { BASE_URL, API_KEY } from "../config";
-import { FAKE_POPULARS } from "./fake-data";
 
 export class TvShowAPI {
   static async fetchPopulars() {
